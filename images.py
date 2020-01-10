2bca965e5a4e4664391033f59b44dd8627676090
Mateusz Rezler <mateuszrezler@gmail.com>
2020-01-10 11:13:36 +0100

    Addition of try/except block catching HTTPError

--- images.py
+++ images.py
@@ -1,5 +1,6 @@
 from keras.preprocessing.image import load_img
 from numpy.random import randint
+from urllib.error import HTTPError
 from urllib.request import urlretrieve
 
 
@@ -12,8 +13,11 @@ def load_random_images(amount=10, length=360, width=360):
     for image in range(amount):
         image_nr = randint(1, 1084+1)
         url = f'https://i.picsum.photos/id/{image_nr}/{length}/{width}.jpg'
-        urlretrieve(url, f'{image_nr}.jpg')
-        images.append(load_img(f'{image_nr}.jpg'))
+        try:
+            urlretrieve(url, f'{image_nr}.jpg')
+            images.append(load_img(f'{image_nr}.jpg'))
+        except HTTPError:
+            pass
 
     return images
 
