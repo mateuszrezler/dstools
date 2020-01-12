577d416414e4d336af08111109207dba3e540656
Mateusz Rezler <mateuszrezler@gmail.com>
2020-01-12 23:28:32 +0100

    Addition of 'last_image_number' parameter

--- images.py
+++ images.py
@@ -4,20 +4,17 @@ from urllib.error import HTTPError
 from urllib.request import urlretrieve
 
 
-def load_random_images(amount=10, length=360, width=360):
-
+def load_random_images(amount=10, length=360, width=360,
+                       last_image_number=1084):
     """Load random images from `https://picsum.photos/` website"""
-
     images = []
-
     while len(images) < amount:
-        image_nr = randint(1, 1084+1)
-        url = f'https://i.picsum.photos/id/{image_nr}/{length}/{width}.jpg'
+        image_number = randint(1, last_image_number+1)
+        url = f'https://i.picsum.photos/id/{image_number}/{length}/{width}.jpg'
         try:
-            urlretrieve(url, f'{image_nr}.jpg')
-            images.append(load_img(f'{image_nr}.jpg'))
+            urlretrieve(url, f'{image_number}.jpg')
+            images.append(load_img(f'{image_number}.jpg'))
         except HTTPError:
             pass
-
     return images
 
