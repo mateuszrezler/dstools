cebcc2c800ebe2d5f086b9c7eaaa2bb5a796789b
Mateusz Rezler <mateuszrezler@gmail.com>
2020-01-13 22:20:31 +0100

    Addition of 'display_grid_of_images' function

--- images.py
+++ images.py
@@ -1,7 +1,19 @@
 from keras.preprocessing.image import load_img
+from matplotlib.pyplot import axis, figure, imshow, show, subplot
 from numpy.random import randint
 from urllib.error import HTTPError
 from urllib.request import urlretrieve
+
+
+def display_grid_of_images(list_of_images, columns=2, size=1):
+    """Display PIL images as subplots in a grid of a given number of columns"""
+    for index, image in enumerate(list_of_images):
+        if index % columns == 0:
+            figure(figsize=(6.4*size, 4.8*size))
+        subplot(1, columns, index % columns+1)
+        axis('off')
+        imshow(image)
+    show()
 
 
 def load_random_images(amount=10, length=360, width=360,
