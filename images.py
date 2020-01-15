2536a6e4b99d3e917681a8df16b0fe16cebf010b
Mateusz Rezler <mateuszrezler@gmail.com>
2020-01-15 21:12:34 +0100

    Addition of 'titles' parameter

--- images.py
+++ images.py
@@ -1,5 +1,5 @@
 from keras.preprocessing.image import img_to_array, load_img
-from matplotlib.pyplot import axis, figure, imshow, show, subplot
+from matplotlib.pyplot import axis, figure, imshow, show, subplot, title
 from numpy import expand_dims
 from numpy.random import randint
 from skimage.transform import resize
@@ -7,12 +7,14 @@ from urllib.error import HTTPError
 from urllib.request import urlretrieve
 
 
-def display_grid_of_images(list_of_images, columns=2, size=1):
+def display_grid_of_images(list_of_images, columns=2, size=1, titles=[]):
     """Display PIL images as subplots in a grid of a given number of columns"""
     for index, image in enumerate(list_of_images):
         if index % columns == 0:
             figure(figsize=(6.4*size, 4.8*size))
         subplot(1, columns, index % columns+1)
+        if titles:
+            title(titles[index])
         axis('off')
         imshow(image)
     show()
