f0c97a66d30414f67b0a31036c3cb7cd54a1b4cd
Mateusz Rezler <mateuszrezler@gmail.com>
2020-01-18 21:27:48 +0100

    Addition of 'remove_re' function

--- strings.py
+++ strings.py
@@ -9,14 +9,28 @@ def extract_re(text, *expressions):
 
     for expression in expressions:
         found = re.findall(expression, text)
-
         if found:
             sep = ''
-
             if output_str:
                 sep = ', '
-
             output_str += sep + ', '.join(found)
 
-    return output_str
+    return output_str.strip()
+
+
+def remove_re(text, *expressions):
+
+    """Remove regex-captured fragments from text"""
+
+    for expression in expressions:
+        found = re.findall(expression, text)
+        if found:
+            removed = text
+
+            for hit in found:
+                removed = re.sub(expression, '', removed)
+
+            text = removed
+
+    return text.strip()
 
