236c3d2ece1e82b30d2cbf087975c74dbe4db1c7
Mateusz Rezler <mateuszrezler@gmail.com>
2021-03-01 09:16:01 +0100

    Implement a factory of synthetic data

--- torchtools.py
+++ torchtools.py
@@ -1,5 +1,20 @@
+from numpy.random import randn, random
+
+
 def from_np(tensor):
     if len(tensor.shape) == 1:
         tensor = tensor.reshape(-1, 1)
     return from_numpy(tensor.astype(float32))
+
+
+class SyntheticData2D(object):
+
+    def __init__(self,
+                 x_min=-10,
+                 x_max=10,
+                 function=lambda x: x,
+                 n_points=10,
+                 noise=1):
+        self.x = random(n_points)*(x_max-x_min)+x_min
+        self.y = function(self.x+randn(n_points)*noise)
 
