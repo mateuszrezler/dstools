0addcb49ad83dec2ef1faa4e542deb1c97ddfd1d
Mateusz Rezler <mateuszrezler@gmail.com>
2021-03-02 23:38:50 +0100

    Implement getting torch tensor

--- torchtools.py
+++ torchtools.py
@@ -21,6 +21,9 @@ class SyntheticData2D(object):
         self.x = random(n_points)*(x_max-x_min)+x_min
         self.y = function(self.x+randn(n_points)*noise)
 
+    def get_torch(self):
+        return from_np(self.x), from_np(self.y)
+
     def show_scatter(self):
         scatter(self.x, self.y)
         show()
